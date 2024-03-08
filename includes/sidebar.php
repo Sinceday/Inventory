f7632740de473de406c29fd593e53b23d7bc0a13
Zenji Yangco <zenjiyangco@gmail.com>
2024-03-08 18:32:48 +0800

    add user header

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -76,7 +76,11 @@
                 </li>
             </ul>
         </aside>
-
+        <?php include 'header.php'; ?>
+        <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
         <script src="src/js/script.js"></script>
 </body>
 
